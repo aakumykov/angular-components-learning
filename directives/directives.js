86c81eb01a52303e58ac6b0a22527011c01d281e
Андрей Кумыков <aakumykov@yandex.ru>
2016-09-27 11:55:45 +0700

    Как будто бы, включение доступ к глобальному

--- directives/directives.js
+++ directives/directives.js
@@ -3,5 +3,23 @@ var NL = "\n";
 var app = angular.module('MyApp',[]);
 
 app.controller('MyCtrl',function($scope){
-	
+	$scope.card = {
+		title: 'Заголовок',
+		content: 'Содержимое',
+	}
+
+	$scope.test1 = function(){
+		alert(
+			'=test1='+NL+
+			'title: '+$scope.card.title+NL+
+			'content: '+$scope.card.content+NL+
+			''
+		);
+	}
+});
+
+app.directive('w3TestDirective',function(){
+	return {
+		templateUrl: 'include1.html'
+	}
 });
